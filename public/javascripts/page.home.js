12ac894c7e5fa5945790aac5efa2ecf119b7f221
Michael <deadlynighthawk99@gmail.com>
2016-07-01 15:10:06 -0400

    -a

--- public/javascripts/page.home.js
+++ public/javascripts/page.home.js
@@ -1,43 +1,50 @@
+// Display the Header Text
 var current_title_letter = 0;
 var banner_title = "Dancer's Alley";
 
 var current_info_letter = 0;
-var banner_info = "I believe we all have a dancer within ourselves and that dance is an art form of truth and expression. " +
-				"An education creates space that allows the dancer to truly feel the beauty inside oneself, " +
-				"and that inner beauty creates an ease of movement and self confidence in our daily lives. " +
-				"Transfer that to the stage and the audience is treated to a captivating performance! EVERYBODY CAN DANCE!";
-
-if (window.matchMedia("(min-width: 768px)").matches) {
-	setTimeout(displayBannerTitle, 500);
-}
-else {
-	$("#home-banner--title").text(banner_title);
-	$("#home-banner--info").text(banner_info);
-}
+var banner_info = "“ I believe we all have a dancer within ourselves and that dance is an art form of truth and expression. " +
+				  "An education creates space that allows the dancer to truly feel the beauty inside oneself, " +
+				  "and that inner beauty creates an ease of movement and self confidence in our daily lives. " +
+				  "Transfer that to the stage and the audience is treated to a captivating performance! ”";
 
 function displayBannerTitle() {
 	if (banner_title.length >= current_title_letter) {
 		$("#home-banner--title").append(banner_title.charAt(current_title_letter));
 		current_title_letter++;
-		setTimeout(displayBannerTitle, 110);
+		setTimeout(displayBannerTitle, 100);
 	}
 	else {
-		setTimeout(displayBannerInfo, 1500);
+		setTimeout(displayBannerInfo, 500);
 	}
 }
 
 function displayBannerInfo() {
+	bannerTextFromTop();
 	if (banner_info.length >= current_info_letter) {
 		$("#home-banner--info").append(banner_info.charAt(current_info_letter));
 		current_info_letter++;
-		setTimeout(displayBannerInfo, 50);
+		setTimeout(displayBannerInfo, 25);
 	}
 }
 
+if (window.matchMedia("(min-width: 768px)").matches) setTimeout(displayBannerTitle, 500);
+else {
+	$("#home-banner--title").text(banner_title);
+	$("#home-banner--info").text(banner_info);
+}
 
-$("#header").css("position","relative");
-$("#content").css("margin-top","0px");
+//Center the Header Text vertically
+function bannerTextFromTop() {
+	var container_height = $("#home-banner").outerHeight();
+	var text_height = $("#home-banner--text").outerHeight();
+	var text_margin = (container_height / 2) - (text_height / 2);
+	$("#home-banner--text").css("margin-top", text_margin);
+}
 
+$(window).on("resize load", bannerTextFromTop);
+
+//Make Nav Bar Fixed/Relative
 $(window).on("scroll", function() {
 	var fromTop = $("html").scrollTop();
 	var banner = $("#home-banner").height();
@@ -55,12 +62,5 @@ $(window).on("scroll", function() {
 	}
 });
 
-function bannerTextFromTop() {
-	var container_height = $("#home-banner").outerHeight();
-	var text_margin = container_height / 2;
-	if (window.matchMedia("(max-width: 767px)").matches) text_margin = text_margin - 50
-	else text_margin = text_margin - 50
-	$("#home-banner--title").css("margin-top", text_margin);
-}
-
-$(window).on("resize load", bannerTextFromTop);
+$("#header").css("position","relative");
+$("#content").css("margin-top","0px");
