6b05ddbcabcf487b5487b773ae45d01bd9424b30
Michael <deadlynighthawk99@gmail.com>
2016-07-02 19:03:43 -0400

    -a

--- public/javascripts/template.main.js
+++ public/javascripts/template.main.js
@@ -1,7 +1,7 @@
 $("#header-mobile--menu").click(function() {
 	$("#header-mobile--item--holder").toggle();
 	if ($("#header-mobile--item--holder").is(":visible"))
-		$("#header-socialnetworking--holder-mobile").css("margin-top","262px");
+		$("#header-socialnetworking--holder-mobile").css("margin-top",$("#header-mobile").height() + 4);
 	else
 		$("#header-socialnetworking--holder-mobile").css("margin-top","54px");
 });
