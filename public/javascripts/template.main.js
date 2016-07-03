824dd0e88a1ef51763977e325b59513dfeeb3175
Michael <deadlynighthawk99@gmail.com>
2016-07-02 20:44:43 -0400

    -a

--- public/javascripts/template.main.js
+++ public/javascripts/template.main.js
@@ -1,10 +1,10 @@
-// $("#header-mobile--menu").click(function() {
-// 	$("#header-mobile--item--holder").toggle();
-// 	if ($("#header-mobile--item--holder").is(":visible"))
-// 		$("#header-socialnetworking--holder-mobile").css("margin-top",$("#header-mobile").height() + 4);
-// 	else
-// 		$("#header-socialnetworking--holder-mobile").css("margin-top","54px");
-// });
+$("#header-mobile--menu").click(function() {
+	$("#header-mobile--item--holder").toggle();
+	if ($("#header-mobile--item--holder").css("display") == "none" )
+		$("#header-socialnetworking--holder-mobile").css("margin-top","54px");
+	else
+		$("#header-socialnetworking--holder-mobile").css("margin-top",$("#header-mobile").height() + 4);
+});
 
 function wrapperCenter() {
 	if (window.matchMedia("(min-width: 2000px)").matches) {
