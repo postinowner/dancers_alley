59ef42675b68eac5d7f78c2d0766a9f744dad255
Michael <deadlynighthawk99@gmail.com>
2016-07-03 12:04:30 -0400

    -a

--- controllers/index.js
+++ controllers/index.js
@@ -10,4 +10,34 @@ module.exports.set = function(app) {
 							 page_name : 'about'});
 	});
 
+	app.get('/news', function(req, res) {
+		res.render('news', { title : 'News' ,
+							 page_name : 'news'});
+	});
+
+	app.get('/schedules', function(req, res) {
+		res.render('schedules', { title : 'Schedules' ,
+							 page_name : 'schedules'});
+	});
+
+	app.get('/workshops', function(req, res) {
+		res.render('workshops', { title : 'Workshops' ,
+							 page_name : 'workshops'});
+	});
+
+	app.get('/recitalinfo', function(req, res) {
+		res.render('recitalinfo', { title : 'Recital Info' ,
+							 page_name : 'recitalinfo'});
+	});
+
+	app.get('/policies', function(req, res) {
+		res.render('policies', { title : 'Policies' ,
+							 page_name : 'policies'});
+	});
+
+	app.get('/ensemble', function(req, res) {
+		res.render('ensemble', { title : 'Ensemble' ,
+							 page_name : 'ensemble'});
+	});
+
 }
